59cf93b537a540e9f63b65f8925dc2c7747ed69b
Amogh Dodawad <amoghasdodawad@gmail.com>
2024-08-17 00:43:31 +0530

    Checking for file before stream

--- streaming/controllers/hlsController.js
+++ streaming/controllers/hlsController.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 function streamHls(req, res){
     try {
         const { file, videoId } = req.params;
+        const filepath = `${process.env.HLS_DIR}/${videoId}/${file}`;
+        if(!fs.existsSync(filepath)) throw new Error('File doesn\'t exist');
         const readStream = fs.createReadStream(`${process.env.HLS_DIR}/${videoId}/${file}`);
         res.writeHead(200, 'Ok', {
             'Content-Type': file === 'master.m3u8' ? 'application/x-mpegURL' : 'video/MP2T',
